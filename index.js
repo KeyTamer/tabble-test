a4fe1232070e48b8a00a83be954321cad44b01f6
KeyTamer <das.abc@t-online.de>
2018-01-01 03:45:08 +0100

    ++

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hallo Welt");
+const express = require("express");
+const app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+  res.end();
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port '+port);
+});
